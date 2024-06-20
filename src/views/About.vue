1d904ca15015c262fc71250f975a534726734414
Jeffrey Qin <jyqin1@uci.edu>
2024-06-19 22:05:59 -0700

    minor changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="about">
+  <div class="home">
     <h1>About Me</h1>
+    <img :src="profileImage" alt="Profile Picture" class="profile-pic">
     <p>Hello! I'm a dedicated and detail-oriented developer, here is a little about my background, skills, and what I bring to the table.</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AppAbout'
+  name: 'AppAbout',
+  data() {
+    return {
+      profileImage: require('@/assets/pfp2.jpg')
+    }
+  }
 }
 </script>
 
 <style scoped>
-.about {
+.home {
   padding: 20px;
   text-align: center;
+}
+
+.profile-pic {
+  width: 400px;
+  height: 500px;
+  border-radius: 60%;
+  object-fit: cover;
+  object-position: center;
+  margin-bottom: 20px;
 }
 </style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="contact">
     <h1>Contact Me</h1>
-    <p>Feel free to reach out via email at <a href="mailto:email@example.com">email@example.com</a> or follow me on <a href="https://linkedin.com">LinkedIn</a>.</p>
+    <p>Feel free to reach out via email at <a href="mailto:jyqin1@uci.edu">jyqin1@uci.edu</a> or follow me on <a href="https://www.linkedin.com/in/jeffreyqin1">LinkedIn</a>.</p>
   </div>
 </template>
 
