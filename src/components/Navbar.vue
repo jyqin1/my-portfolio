050d23f9a0917fcc5672e65201c163155f83793a
Jeffrey Qin <jyqin1@uci.edu>
2024-08-28 18:29:59 -0700

    navbar update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/projects">Projects</router-link>
-    <router-link to="/contact">Contact</router-link>
+  <nav id="navbar" class="black navbar navbar-expand-lg fixed-top">
+    <router-link id="nav-logo" class="navbar-brand" to="/">Jeffrey Qin</router-link>
+    <button class="navbar-toggler" id="nav-btn" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-btn"><i class="fa fa-bars" aria-hidden="true"></i></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+      <ul id="nav-ul" class="navbar-nav">
+        <li class="nav-item navigation-list">
+          <router-link class="nav-link nav-custom-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item navigation-list">
+          <router-link class="nav-link nav-custom-link" to="/about">About</router-link>
+        </li>
+        <li class="nav-item navigation-list">
+          <router-link class="nav-link nav-custom-link" to="/portfolio">Portfolio</router-link>
+        </li>
+        <li class="nav-item navigation-list">
+          <router-link class="nav-link nav-custom-link" to="/contact">Contact</router-link>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
@@ -13,18 +30,29 @@ export default {
 </script>
 
 <style scoped>
-nav {
-  display: flex;
-  justify-content: space-around;
-  padding: 10px;
-  background-color: #f2f2f2;
+#navbar {
+  font-family: Roboto, sans-serif;
+  background-size: cover;
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 15px 20px;
+  transition: all 0.5s ease;
 }
-router-link {
-  text-decoration: none;
-  padding: 10px;
-  color: #333;
+
+#nav-logo {
+  font-size: 24px;
+  color: #000000;
+  text-transform: uppercase;
+  font-weight: bolder;
 }
-router-link:hover {
-  color: #555;
+
+.nav-custom-link {
+  font-weight: bold;
+  color: #000000 !important;
+  text-transform: uppercase;
+}
+
+.nav-custom-link:hover {
+  color: #FFFFFF !important;
+  background-color: #000000;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+createApp(App).use(router).mount('#app')
